4bc89015d64d6dbdfc1149c0aea2f91332d1b4bf
centan <centan@outlook.com>
2026-04-20 15:40:20 +0300

    fix: provide fallback projectId for legacy sanity client to prevent CI test failures

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,8 +1,8 @@
 import {createClient} from '@sanity/client'
 
 export const sanityClient = createClient({
-  projectId: import.meta.env['VITE_SANITY_PROJECT_ID'],
-  dataset: import.meta.env['VITE_SANITY_DATASET'],
+  projectId: import.meta.env['VITE_SANITY_PROJECT_ID'] || 'wn3a082f',
+  dataset: import.meta.env['VITE_SANITY_DATASET'] || 'production',
   apiVersion: import.meta.env['VITE_SANITY_API_VERSION'] || '2025-01-01',
-  useCdn: false,
+  useCdn: true,
 })
